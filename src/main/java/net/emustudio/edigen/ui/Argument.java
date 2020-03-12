47883b7f7c0ee1387053d31bab1f3b6ddd1ad554
Peter Jakubčo <pjakubco@gmail.com>
2020-03-12 07:23:40 +0100

    Fixed some javadoc + bumped versions of some maven plugins

--- src/main/java/net/emustudio/edigen/ui/Argument.java
+++ src/main/java/net/emustudio/edigen/ui/Argument.java
@@ -24,23 +24,24 @@ import java.util.regex.Pattern;
 
 /**
  * A command line argument specification.
- * 
+ * <p>
  * This does not represent a specific argument instance obtained from the user,
  * but information about the possible argument, like his name and description.
+ *
  * @author Matúš Sulír
  */
 public class Argument {
 
     /**
      * An argument type.
-     * 
-     * <p>If <tt>arg1</tt> is a mandatory argument, <tt>-arg2 value2</tt> is a
-     * value and <tt>-arg3</tt> is a flag, this is an example of a valid
+     * <p>
+     * If {@code arg1} is a mandatory argument, {@code -arg2 value2} is a
+     * value and {@code -arg3} is a flag, this is an example of a valid
      * command line input:
      * <pre><kbd>program arg1 -arg2 value2 -arg3</kbd></pre>
      * The order of value and flag arguments is irrelevant. Mandatory arguments
      * must occur in the specified order, but they can interleave with other
-     * parameters.</p>
+     * parameters.
      */
     public enum Type {
         /**
@@ -48,17 +49,17 @@ public class Argument {
          */
         MANDATORY,
         /**
-         * An argument with one value (for example <tt>-v value</tt>).
+         * An argument with one value (for example {@code -v value}).
          */
         VALUE,
         /**
-         * An argument without any value (a flag, e.g. <tt>-f</tt>).
+         * An argument without any value (a flag, e.g. {@code -f}).
          */
         FLAG
     }
-    
+
     private static final Pattern HELP_VALUE = Pattern.compile("<\\w+>");
-    
+
     private final Type type;
     private final String option;
     private final String value;
@@ -67,33 +68,35 @@ public class Argument {
 
     /**
      * Constructs an unnamed mandatory argument.
+     *
      * @param description the description used in the help text
-     * @param key the key to which the recognized value will be later assigned
+     * @param key         the key to which the recognized value will be later assigned
      */
     public Argument(String description, Setting key) {
         type = Type.MANDATORY;
         option = "";
-        
+
         Matcher matcher = HELP_VALUE.matcher(description);
 
         if (matcher.find())
             value = matcher.group();
         else
             value = "<value>";
-        
+
         this.description = description;
         this.key = key;
     }
-    
+
     /**
      * Constructs a flag or a value argument.
-     * @param option the option name, e.g. "o" for "-o"
+     *
+     * @param option      the option name, e.g. "o" for "-o"
      * @param description the description used in the help text
-     * @param key the key to which the recognized value will be later assigned
+     * @param key         the key to which the recognized value will be later assigned
      */
     public Argument(String option, String description, Setting key) {
         Matcher matcher = HELP_VALUE.matcher(description);
-        
+
         if (matcher.find()) {
             type = Type.VALUE;
             value = matcher.group();
@@ -101,7 +104,7 @@ public class Argument {
             type = Type.FLAG;
             value = "";
         }
-        
+
         this.option = option;
         this.description = description;
         this.key = key;
@@ -109,14 +112,16 @@ public class Argument {
 
     /**
      * Returns the argument type.
+     *
      * @return the type
      */
     public Type getType() {
         return type;
     }
-    
+
     /**
      * Returns the option name, e.g. "o" for "-o"
+     *
      * @return the option name, or an empty string if the argument is unnamed
      */
     public String getOption() {
@@ -125,17 +130,19 @@ public class Argument {
 
     /**
      * Returns the value name suitable for printing in a help text,
-     * e.g. "<directory>". The value is automatically extracted from the
+     * e.g. {@code <directory>}. The value is automatically extracted from the
      * description.
-     * @return the value including the brackets; "<value>" in case of extraction
-     *         failure for a mandatory argument; an empty string for a flag
+     *
+     * @return the value including the brackets; {@code <value>} in case of extraction
+     * failure for a mandatory argument; an empty string for a flag
      */
     public String getValue() {
         return value;
     }
-    
+
     /**
      * Returns the description used in the help text.
+     *
      * @return the description
      */
     public String getDescription() {
@@ -144,6 +151,7 @@ public class Argument {
 
     /**
      * Returns the key to which the recognized value will be later assigned.
+     *
      * @return the key
      */
     public Setting getKey() {
