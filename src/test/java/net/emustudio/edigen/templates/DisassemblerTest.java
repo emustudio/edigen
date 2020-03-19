504b5bf7cc290e350f2a7c101dd8fe4125065eee
Peter Jakubčo <pjakubco@gmail.com>
2020-03-19 14:26:44 +0100

    Added shift_left and shift_right decoding strategies

--- src/test/java/net/emustudio/edigen/templates/DisassemblerTest.java
+++ src/test/java/net/emustudio/edigen/templates/DisassemblerTest.java
@@ -2,6 +2,7 @@ package net.emustudio.edigen.templates;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class DisassemblerTest {
@@ -12,6 +13,20 @@ public class DisassemblerTest {
         assertEquals(127, absolute(new Byte[] { (byte)0x81 })[0].byteValue());
     }
 
+    @Test
+    public void testBitReverseWorks() {
+        assertEquals((byte)0b11100000, bit_reverse(new byte[] { 0b000111})[0]);
+    }
+
+    @Test
+    public void testShiftLeftWorks() {
+        assertArrayEquals(new byte[] { 2, 4 }, shift_left(new byte[] { 1,2 }));
+    }
+
+    @Test
+    public void testShiftRightWorks() {
+        assertArrayEquals(new byte[] { 0, (byte)0b10000001 }, shift_right(new byte[] { 1,2 }));
+    }
 
     public static Byte[] absolute(Byte[] value) {
         Byte[] result = new Byte[value.length];
@@ -31,6 +46,44 @@ public class DisassemblerTest {
             } else {
                 result = value;
             }
+        }
+        return result;
+    }
+
+    public static byte[] bit_reverse(byte[] value) {
+        byte[] result = new byte[value.length];
+
+        for (int octet = 0; octet < result.length; octet++) {
+            for (int bit = 0; bit < 8; bit++) {
+                result[octet] |= (value[octet] & (1 << bit)) >>> bit << (8 - bit - 1);
+            }
+        }
+
+        return result;
+    }
+
+    public static byte[] shift_left(byte[] value) {
+        byte[] result = new byte[value.length];
+
+        for (int octet = 0; octet < result.length; octet++) {
+            int shifted = (value[octet] << 1) & 0xFF;
+            if (octet + 1 < result.length) {
+                shifted |= value[octet + 1] >>> 7;
+            }
+            result[octet] = (byte)shifted;
+        }
+        return result;
+    }
+
+    public static byte[] shift_right(byte[] value) {
+        byte[] result = new byte[value.length];
+
+        for (int octet = result.length - 1; octet >= 0; octet--) {
+            int shifted = (value[octet] >>> 1) & 0xFF;
+            if (octet - 1 >= 0) {
+                shifted |= ((value[octet - 1] & 1) << 7);
+            }
+            result[octet] = (byte)shifted;
         }
         return result;
     }
