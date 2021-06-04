2ac52fc1f0d2c2f60532c5a90448a3116967ba00
Peter Jakubco <pjakubco@gmail.com>
2021-06-04 18:27:53 +0200

    [#26] Add unit tests for SemanticCheckVisitor

--- src/test/java/net/emustudio/edigen/passes/SemanticCheckVisitorTest.java
+++ src/test/java/net/emustudio/edigen/passes/SemanticCheckVisitorTest.java
@@ -1,16 +1,73 @@
 package net.emustudio.edigen.passes;
 
+import net.emustudio.edigen.SemanticException;
+import net.emustudio.edigen.nodes.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class SemanticCheckVisitorTest {
+    private Decoder decoder;
+    private Variant variant;
+    private Disassembler disassembler;
+    private Format format;
 
-    // subrule in the middle of rule cannot be without length
-    //  (only if their definition has length defined)
+    @Before
+    public void setUp() {
+        decoder = new Decoder();
+        Rule rule = new Rule("rule");
+        decoder.addChild(rule);
+        variant = new Variant();
+        rule.addChild(variant);
 
-    // subrule at the end (last one) in the rule can have no length
-    // (why? how is it inferred?)
+        disassembler = new Disassembler();
+        format = new Format("");
+        disassembler.addChild(format);
+    }
 
-    // values in rules must be "returning" - return a value
+    @Test(expected = SemanticException.class)
+    public void testNotEndingSubruleMustHaveLengthSpecified() throws SemanticException {
+        variant.addChild(new Subrule("subrule1"));
+        variant.addChild(new Subrule("subrule2"));
+        decoder.accept(new SemanticCheckVisitor());
+    }
 
-    // set of values in any two disassembler formats should not be the same
+    @Test
+    public void testEndingSubruleDoesntNeedToHaveLengthSpecified() throws SemanticException {
+        variant.addChild(new Subrule("subrule1"));
+        decoder.accept(new SemanticCheckVisitor());
+    }
 
+    @Test(expected = SemanticException.class)
+    public void testNonReturningValueUsedInDisassemblerThrows() throws SemanticException {
+        Value value = new Value("rule");
+        format.addChild(value);
+        Specification specification = new Specification(decoder, disassembler);
+        specification.accept(new ResolveNamesVisitor());
+        specification.accept(new SemanticCheckVisitor());
+    }
 
+    @Test
+    public void testReturningValueCanBeUsedInDisassembler() throws SemanticException {
+        Value value = new Value("rule");
+        format.addChild(value);
+        variant.setReturnString("at least something");
+        Specification specification = new Specification(decoder, disassembler);
+        specification.accept(new ResolveNamesVisitor());
+        specification.accept(new SemanticCheckVisitor());
+    }
+
+    @Test(expected = SemanticException.class)
+    public void testTwoValuesUsedInFormatCannotBeTheSame() throws SemanticException {
+        Value value = new Value("rule");
+        format.addChild(value);
+
+        Format format2 = new Format("");
+        format2.addChild(value);
+        disassembler.addChild(format2);
+
+        variant.setReturnString("at least something");
+        Specification specification = new Specification(decoder, disassembler);
+        specification.accept(new ResolveNamesVisitor());
+        specification.accept(new SemanticCheckVisitor());
+    }
 }
